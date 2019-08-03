62222a98f61ebfc362d65edc53af097d69bf6225
Priyesh <ekthapriyesh24@gmail.com>
2019-08-04 03:51:34 +0530

    Updates

---
+++ precache-manifest.50b78d04c4f77aa3a3501528064e95de.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81d336c76a7c9b757f5fd5bd6a7ce997",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca899909186ba3d361e8",
+    "url": "/static/css/2.8e9a764b.chunk.css"
+  },
+  {
+    "revision": "1c1c061782965652e48c",
+    "url": "/static/css/main.db8844fb.chunk.css"
+  },
+  {
+    "revision": "ca899909186ba3d361e8",
+    "url": "/static/js/2.3122c697.chunk.js"
+  },
+  {
+    "revision": "1c1c061782965652e48c",
+    "url": "/static/js/main.b3e7ddd1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6eded0dc7b13730f3a4c1feaee6973d8",
+    "url": "/static/media/back5.6eded0dc.jpg"
+  },
+  {
+    "revision": "435cf0a59775e5cf1ca5d035ec9c0a8f",
+    "url": "/static/media/cal.435cf0a5.jpeg"
+  },
+  {
+    "revision": "e4c5c4c6cae7aecaf5c1bbeade407a50",
+    "url": "/static/media/chic1.e4c5c4c6.jpg"
+  },
+  {
+    "revision": "da489316a47d18b7af5a93e80fad07eb",
+    "url": "/static/media/mypic2.da489316.jpg"
+  },
+  {
+    "revision": "2182330384ebbad826821fdf8e84efe5",
+    "url": "/static/media/react-bootstrap-carousel.21823303.eot"
+  },
+  {
+    "revision": "2fe6d179ea5aa1ceab26113e77e13da4",
+    "url": "/static/media/react-bootstrap-carousel.2fe6d179.svg"
+  },
+  {
+    "revision": "c746c4882df00af3b0a7a3b1616bf295",
+    "url": "/static/media/react-bootstrap-carousel.c746c488.woff"
+  },
+  {
+    "revision": "f7448c0408e7a78012a82def0fb9e2fe",
+    "url": "/static/media/react-bootstrap-carousel.f7448c04.ttf"
+  },
+  {
+    "revision": "807c1ea88902d17d493adebf849274fa",
+    "url": "/static/media/syn.807c1ea8.png"
+  }
+]);
